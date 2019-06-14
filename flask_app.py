74a3cb6fcf9584d8bdee7a05aa181eb12729d032
JWolff <jwolff@dsdmail.net>
2019-06-14 05:22:06 +0000

    Got submission to work with material card.

--- flask_app.py
+++ flask_app.py
@@ -30,6 +30,7 @@ class Comment(db.Model):
     __tablename__ = "comments"
 
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(4096))
     content = db.Column(db.String(4096))
 
 @app.route("/", methods=["GET", "POST"])
@@ -37,7 +38,7 @@ def index():
     if request.method == "GET":
         return render_template("main_page.html", comments=Comment.query.all())
 
-    comment = Comment(content=request.form["contents"])
+    comment = Comment(title=request.form["title"], content=request.form["contents"])
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('index'))
