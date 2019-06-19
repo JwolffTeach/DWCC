0ab659cdc286299614bf9fe728916ce8670940a4
JWolff <jwolff@dsdmail.net>
2019-06-19 05:27:48 +0000

    Committing before I delete migrations

--- app/models.py
+++ app/models.py
@@ -1,18 +1,78 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class Comment(db.Model):
+class Comment(db.Model):  # Class Card. Should find a way to rename later.
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(4096))
     content = db.Column(db.String(4096))
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    heroes = db.relationship('Hero', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    hero_name = db.Column(db.String(4096))
+    hero_class = db.Column(db.String(4096))
+    hero_race = db.Column(db.String(4096))
+    hero_alignment = db.Column(db.String(4096))
+    hero_looks = db.relationship('Hero_Looks', backref='hero', lazy='dynamic')
+
+    def __repr__(self):
+        return '<id {}, owner_id {}, name {}, hero_class {}, race {}, alignment {}.'.format(self.id, self.owner_id, self.hero_name, self.hero_class, self.hero_race, self.hero_alignment)
+
+
+class Hero_Looks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
+    eyes = db.Column(db.String(4096))
+    hair = db.Column(db.String(4096))
+    clothing = db.Column(db.String(4096))
+    body = db.Column(db.String(4096))
+    skin = db.Column(db.String(4096))
+    symbol = db.Column(db.String(4096))
+
+    def __repr__(self):
+        return '<id {}, hero_id {}, eyes {}, hair {}, clothing {}, body {}, skin {}, symbol {}.'.format(self.id, self.hero_id, self.eyes, self.hair, self.clothing, self.body, self.skin, self.symbol)
+
+
+class LKUPLooks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String(4096))
+    look_type = db.Column(db.String(4096))
+    look_details = db.Column(db.String(4096))
+
+    def __repr__(self):
+        return '<id {}, class_name {}, look_type {}, look_details {}'.form(self.id, self.class_name, self.look_type, self.look_details)
+
+
+class LKUPAlignment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String(4096))
+    alignment_name = db.Column(db.String(4096))
+    
+    def __repr__(self):
+        return '<id {}, class_name {}, alignment_name {}'.form(self.id, self.class_name, self.alignment_name)
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,20 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm
-from app.models import Comment, User
+from app.forms import LoginForm, RegistrationForm
+from app.models import Comment, User, Hero
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+@login_required
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("main_page.html", heroes=current_user.heroes.all(), title='Dungeon World Character Creator')
 
-    comment = Comment(title=request.form["title"], content=request.form["contents"])
-    db.session.add(comment)
+    hero = Hero(owner_id=current_user.id, hero_name=request.form["hero_name"], hero_class=request.form["hero_class"])
+    db.session.add(hero)
     db.session.commit()
     return redirect(url_for('index'))
 
@@ -22,9 +26,44 @@ def newmain():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/builder', methods=['GET', 'POST'])
+@login_required
+def builder():
+    return render_template('builder.html', title='Build a Character')
